7b4eaac39c314e6d098554fc1390785930568358
Abdullah <uzbekcoder.2002@gmail.com>
2022-03-05 20:23:51 +0500

    Edit views

--- translator/views.py
+++ translator/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from .models import Dictionary  # import the model
 # Create your views here.
 
 
 def homePage(request):
-    return render(request, 'home.html')
+    query = request.GET.get("q", "")
+    if query and query != "":
+        result = Dictionary.objects.filter(english__contains=query).all()[:3]
+        for data in result:
+            print(data.uzbek)
+    else:
+        result = None
+    return render(request, 'home.html', {"q": query, "result": result})

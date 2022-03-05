f2d8a3da25be4063da367367796583b7445a0cb9
Abdullah <uzbekcoder.2002@gmail.com>
2022-03-05 20:18:18 +0500

    Creating models for dictionary

--- translator/models.py
+++ translator/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Dictionary(models.Model):
+
+    english = models.CharField("English word", max_length=100)
+    uzbek = models.CharField("Uzbek word", max_length=100)

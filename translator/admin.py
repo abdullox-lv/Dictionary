a8aff830d7b1494635218fd863260f524208d386
Abdullah <uzbekcoder.2002@gmail.com>
2022-03-05 20:20:39 +0500

    Creating superuser and Admin class

--- translator/admin.py
+++ translator/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Dictionary
 # Register your models here.
+
+
+class DictionaryAdmin(admin.ModelAdmin):
+    list_display = ["english", 'uzbek']
+
+
+admin.site.register(Dictionary, DictionaryAdmin)
+
